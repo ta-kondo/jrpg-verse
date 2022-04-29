ff886301d722abf551fe3292ecff3b9fb0ea5e08
ta_kondo <jad.kondo@gmail.com>
2022-04-29 20:27:14 +0900

    [追加] node.jsコンテナを準備。VSCodeのRemote-Containerで開発するイメージ。

---
+++ frontend/docker/start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# --------------------------------------------------------------------
+
+echo -----------------------------------------------------------------
+echo -- Start
+echo -----------------------------------------------------------------
+
+# install from package.json
+# npm install
+
+# start development server
+# npm run dev
+
+# infinite loop !
+tail -f /dev/null
